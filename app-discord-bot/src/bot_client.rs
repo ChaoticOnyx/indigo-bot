6102f90c0b483b8188fda88762699dfaefaf626c
Igor Spichkin <igorsa.ux@outlook.com>
2022-12-16 15:41:31 +0300

    fix: use all intents

--- app-discord-bot/src/bot_client.rs
+++ app-discord-bot/src/bot_client.rs
@@ -14,16 +14,10 @@ impl BotClient {
         trace!("run");
 
         let config = DiscordConfig::get().await.unwrap();
-        let mut client = Client::builder(
-            &config.token,
-            GatewayIntents::non_privileged()
-                | GatewayIntents::GUILD_MESSAGES
-                | GatewayIntents::MESSAGE_CONTENT
-                | GatewayIntents::GUILD_MESSAGE_REACTIONS,
-        )
-        .event_handler(Handler)
-        .await
-        .unwrap();
+        let mut client = Client::builder(&config.token, GatewayIntents::all())
+            .event_handler(Handler)
+            .await
+            .unwrap();
 
         client.start().await.unwrap();
     }
