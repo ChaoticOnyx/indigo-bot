4caf9b43de8c83e264e16a648fc606660c4958d1
Igor Spichkin <igorsa.ux@outlook.com>
2023-01-10 17:34:14 +0300

    feat: navbar, redesign

--- app-http-server/src/endpoints/www/account/mod.rs
+++ app-http-server/src/endpoints/www/account/mod.rs
@@ -1,12 +1,20 @@
-use actix_web::Scope;
+use actix_http::{header, StatusCode};
+use actix_web::{web, HttpResponseBuilder, Responder, Scope};
 
 mod integrations_menu;
 mod profile_menu;
 mod sessions_menu;
+
+async fn redirect() -> impl Responder {
+    HttpResponseBuilder::new(StatusCode::TEMPORARY_REDIRECT)
+        .insert_header((header::LOCATION, "/account/profile"))
+        .finish()
+}
 
 pub fn endpoint() -> Scope {
     Scope::new("/account")
         .service(profile_menu::endpoint)
         .service(sessions_menu::endpoint)
         .service(integrations_menu::endpoint)
+        .default_service(web::to(redirect))
 }
--- app-http-server/src/endpoints/www/index.rs
+++ app-http-server/src/endpoints/www/index.rs
@@ -1,14 +1,10 @@
-﻿use actix_http::header;
-use actix_web::http::StatusCode;
-use actix_web::{get, HttpResponseBuilder, Responder};
+﻿use actix_web::{get, Responder};
 
+use crate::html_response::HtmlResponse;
 use app_shared::prelude::*;
 
 #[instrument]
-#[get("")]
+#[get("/")]
 pub async fn endpoint() -> impl Responder {
-    // HtmlResponse::from_template("index.html", None).await
-    HttpResponseBuilder::new(StatusCode::TEMPORARY_REDIRECT)
-        .insert_header((header::LOCATION, "/404"))
-        .finish()
+    HtmlResponse::from_template("index.html", None::<()>).await
 }
